7760ba253b94e223e74f40be9e34197095da29dc
shriniwasalle <shriniwasalle4@gmail.com>
2026-03-27 22:11:10 +0530

    xpath axes

--- tests/shriniwas/05-xpath.spec.ts
+++ tests/shriniwas/05-xpath.spec.ts
@@ -39,3 +39,74 @@ test("Verify relative xpath", async ({ page }) => {
 
   await search.fill("Books");
 });
+
+// and 
+//input[@id='small-searchterms'][@value='Search store']
+//input[@id='small-searchterms' and @value='Search store']
+
+// or
+// input[@id='small-searchterms123' or @value='Search store']
+
+// Methods
+// 1. text()
+//a[text()="Register"]
+// <h1>This is Heading</h1>
+
+// 2. normalize-space(): It will find the element by removing the spaces in text
+//a[normalize-space(text())="Register"]
+
+// 3. contains()
+//span[contains(text(), "Shopping")]
+//div[contains(@id, "flyout")]
+// <a>Click on Google</a>
+// <a>Clicking Google Link</a>
+// <a>Google</a>
+
+// 4. starts-with()
+// <a data-testid="ABC123">Shri123</a>
+// <a data-testid="ABC789">Shri456</a>
+// <a data-testid="ABC456">Shri897</a>
+//a[starts-with(text(), "Shri")]
+//a[starts-with(@data-testid, "ABC")]
+
+// 5. position()
+// //ul[@class='top-menu']/li[position()=1]
+
+// 6. last()
+//ul[@class='top-menu']/li[last()]
+
+// -------------- XPATH AXES -----------
+// child, parent, following-sibling, preceding-sibling
+// descendant, descendant-or-self
+// ancestor, ancestor-or-self
+
+// Syntax: //tagName[@attributeName="value"]/xpathAxes::tagName[@attributeName="value"]
+/*
+<div>
+  <div>
+    <input>
+      <a href=""> </a>
+      <img href=""> </img>
+      <div href=""> </div>
+    </input>
+  </div>
+  <div>
+    <input> </input>
+  </div>
+  <div>
+    <input> </input>
+  </div>
+</div>
+
+Child: //div[@class="header-menu"]/child::ul[@class="top-menu"]
+Parent: //div[@id="mob-menu-button"]/parent::div
+//div[@id="mob-menu-button"]/../../.. 
+Use /.. to go one step back from the current element
+
+
+
+*/
+
+
+
+
