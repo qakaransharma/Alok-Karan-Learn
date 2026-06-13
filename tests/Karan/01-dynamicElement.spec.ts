d3a3a7d66bba9876efd4414f95eb4d826c19742c
qakaransharma <qa.karansharma@gmail.com>
2026-06-13 23:47:44 +0530

    puhing new data on new branch of adding setTimeOut

--- tests/Karan/01-dynamicElement.spec.ts
+++ tests/Karan/01-dynamicElement.spec.ts
@@ -5,4 +5,12 @@ test("Verify dropdown", async({page})=> {
      await ddlCountry.selectOption("United Kingdom");
    
     //await countryDropdown.selectOption({label: "United States"});
+});
+
+test("Verify dropdown1", async({page})=> {
+    await page.goto("https://testautomationpractice.blogspot.com/");
+     const ddlCountry = page.locator("#country");
+     await ddlCountry.selectOption("India");
+   
+    //await countryDropdown.selectOption({label: "United States"});
 })
